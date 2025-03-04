ae6856844d01ebb68ab20de587a2221dfdc28197
Vikas <vikas.boga@brillio.com>
2025-03-04 12:56:50 +0530

    comit

--- userModule/app.js
+++ userModule/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: "http://54.88.31.60:80", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 

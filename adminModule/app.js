1ddc1b8759e3d8dd66b41ebb2990175907170d09
Vikas <vikas.boga@brillio.com>
2025-03-04 14:31:51 +0530

    cors update

--- adminModule/app.js
+++ adminModule/app.js
@@ -7,7 +7,13 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: "http://54.88.31.60:80", credentials: true }));
+//app.use(cors({ origin: "http://54.88.31.60:80", credentials: true }));
+
+app.use(cors({
+    origin: '*',  // Allow all origins (you can specify allowed origins)
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(cookieParser());
 
